b5e2eeaed54393228e4ddfa8922fc70a4ce8aae5
alejojimenez1299 <alejo.jimenez1299@gmail.com>
2022-11-20 02:13:00 -0500

    cambios estilos menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -14,7 +14,7 @@ export const Menu = (props) => {
 
   function cerrarSesion(){
     document.getElementById("caja_menu").style.display = "none";
-    document.getElementById("form_login").style.display = "block";
+    document.getElementById("login").style.display = "block";
     document.getElementById("txtusu").value = "";
     document.getElementById("txtpas").value = "";
     document.getElementById("txtusu").focus();
@@ -42,24 +42,22 @@ export const Menu = (props) => {
         
     <div id="caja_menu" style={{textAlign:"left"}}>
 
-    <strong className="h3">
-      Bienvenido Usuario : { props.usu.toUpperCase() }
-    </strong>
+
               
-    <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{marginTop:20}}>
+    <nav className="navbar navbar-expand-lg navbar-light bg-primary" >
       <div className="container-fluid">
 
-        <label className="navbar-brand  h5" href=" ">Menú Principal</label>
+        <label className="navbar-brand  h5" style={{color:'white'}} href=" ">Menú Principal</label>
         
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <NavLink to="" className="nav-link  h5  text-center"  onClick={ op_registrar } >Crear Orden</NavLink>
-            <NavLink to="" className="nav-link  h5  text-center"  onClick={ op_listar } >Lista Ordenes</NavLink>
-            <NavLink to="" className="nav-link  h5  text-center"  onClick={ op_actualizar } >Actualizacion Ordenes</NavLink>
-            <a className="nav-link  h5  text-center"  style={{color:"blue"}}  href=" "  onClick={ cerrarSesion } >Cerrar Sesión</a>
+            <NavLink to="" className="nav-link  h5  text-center " style={{color:'white'}} onClick={ op_registrar } >Crear Orden</NavLink>
+            <NavLink to="" className="nav-link  h5  text-center" style={{color:'white'}} onClick={ op_listar } >Lista Ordenes</NavLink>
+            <NavLink to="" className="nav-link  h5  text-center" style={{color:'white'}}  onClick={ op_actualizar } >Actualizacion Ordenes</NavLink>
+            <a className="nav-link  h5  text-center" style={{color:'white'}}  onClick={ cerrarSesion } >Cerrar Sesión</a>
           </div>
         </div>
       </div>
